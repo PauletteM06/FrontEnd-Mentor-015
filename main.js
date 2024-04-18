1c409f47790a3e62f2cee98923d15a2e9ee095bf
PauletteM06 <165092917+PauletteM06@users.noreply.github.com>
2024-04-18 11:09:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const shareBtn = document.querySelector(".button");
+const shareIcons = document.querySelector(".share-icons");
+
+shareBtn.addEventListener("click", ()=> {
+    shareIcons.classList.toggle("show")
+    
+});
+
